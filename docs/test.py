b7364a93bbabaddc22c67d633ee028bf8fa5e10c
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2024-12-11 12:50:24 +0900

    Update test.py

--- docs/test.py
+++ docs/test.py
@@ -5,11 +5,11 @@ def hoge():
     print('ふが')
     print(window.aaa);
     
-def fft(x):
-    for i in x:
-        print(i);
-    y = abs(np.fft.fft(x))
-    print(y)
-    return y.tolist()
+def f(param):
+    return [ param[1] * np.sin( 2*np.pi*param[2] *i + 2*np.pi*param[3]/180 ) for i in range(param[0]) ]
 
-__export__ = ['hoge', 'fft', ]
+def fft(x):
+    return abs(np.fft.fft(x)).tolist()
+
+
+__export__ = ['hoge', 'f', 'fft' ]
