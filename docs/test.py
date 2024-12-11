5cff5785e57eebed0d4e24afc24167e80ae18e32
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2024-12-11 14:49:23 +0900

    Update test.py

--- docs/test.py
+++ docs/test.py
@@ -8,8 +8,15 @@ def hoge():
 def f(param):
     return [ int(param[1]) * np.sin( 2*np.pi*int(param[2]) *i + 2*np.pi*int(param[3])/180 ) for i in range(int(param[0])) ]
 
+def sin(param):
+    A = float(param[0])
+    w = 2*np.pi*float(param[1])
+    t = float(param[2])
+    psi = 2*np.pi*float(param[3])/180
+    return A*np.sin( w*t + psi )
+
 def fft(x):
     return abs(np.fft.fft(x)).tolist()
 
 
-__export__ = ['hoge', 'f', 'fft' ]
+__export__ = ['hoge', 'f', 'sin', 'fft' ]
