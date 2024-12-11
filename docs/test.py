95105f56da89aaba9d43bfd17a5b6531c21d271c
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2024-12-11 12:55:08 +0900

    Update test.py

--- docs/test.py
+++ docs/test.py
@@ -6,7 +6,7 @@ def hoge():
     print(window.aaa);
     
 def f(param):
-    return [ param[1] * np.sin( 2*np.pi*param[2] *i + 2*np.pi*param[3]/180 ) for i in range(param[0]) ]
+    return [ int(param[1]) * np.sin( 2*np.pi*int(param[2]) *i + 2*np.pi*int(param[3])/180 ) for i in range(int(param[0])) ]
 
 def fft(x):
     return abs(np.fft.fft(x)).tolist()
