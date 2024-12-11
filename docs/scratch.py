00b5d09f16d51506b5fd9a048085a9e3d5ec278b
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2024-12-11 18:15:18 +0900

    Update scratch.py

--- docs/scratch.py
+++ docs/scratch.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-def f(parameter):
-    N = int(parameter[0])
-    return [ 2*np.sin(2*np.pi/4*i)+3*np.cos(2*np.pi/8*i) for i in range(N) ]
+def f(N):
+    return [ 2*np.sin(2*np.pi/4*i)+3*np.cos(2*np.pi/8*i) for i in range(int(N)) ]
 
 def fft(x):
     return abs(np.fft.fft(x)).tolist()
-
 
 __export__ = ['f', 'fft']
