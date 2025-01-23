ce8303fbf79639d14de9c780a3858a81a852a2f5
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2025-01-23 12:24:13 +0900

    test

--- docs/scratch.py
+++ docs/scratch.py
@@ -3,9 +3,6 @@ import numpy as np
 def f(N):
     return [ 2*np.sin(2*np.pi/4*i)+3*np.cos(2*np.pi/8*i) for i in range(int(N)) ]
 
-def g(N):
-    return [ -i for i in range(int(N)) ]
-
 def powerspectrum(x):
     return (np.abs(np.fft.fft(x))**2).tolist()
 
