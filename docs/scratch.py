c65eb524f610b547c25edcbbb32f8b6611037093
Tokairin Tomoya <tmytokai@yahoo.co.jp>
2025-01-23 12:18:11 +0900

    test

--- docs/scratch.py
+++ docs/scratch.py
@@ -4,7 +4,7 @@ def f(N):
     return [ 2*np.sin(2*np.pi/4*i)+3*np.cos(2*np.pi/8*i) for i in range(int(N)) ]
 
 def g(N):
-    return [ i*2 for i in range(int(N)) ]
+    return [ -i for i in range(int(N)) ]
 
 def powerspectrum(x):
     return (np.abs(np.fft.fft(x))**2).tolist()
